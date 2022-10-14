697fe5c512c9033938f0a81075a017760f001048
Sejmou <samo.kolter@gmail.com>
2022-10-15 01:36:35 +0200

    hacky solution for imgur auth flow

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,25 @@
-import { printLine } from './modules/print';
-
-console.log('Content script works!');
+console.log('Content script running!');
 console.log('Must reload extension for modifications to take effect.');
 
-printLine("Using the 'printLine' function from the Print Module");
+// this content script should only run when we are on the imgur main site (https://imgur.com)
+// this site is also reached every time an auth flow finishes
+// in this case, we will get the auth token and refresh token from the URL
+// only if the auth_token is present, this content script actually does something with side-effects: it stores the obtained auth token and other data in the extension's local storage
+console.log('current URL', window.location.href);
+const url = window.location.href;
+// problem: the auth and refresh token should be part of the URL but aren't URL query params
+// so, we use this regex to match the key=value syntax
+const paramRegex = /(\w+)=(\w+)/g;
+// ... then, extract the two groups that are part of each map
+const urlKeysAndVals = [...url.matchAll(paramRegex)].map((arr) => arr.slice(1));
+// ... and finally put the keys and values into an object
+const urlKeyValObj = {};
+urlKeysAndVals.forEach(([key, val]) => {
+  urlKeyValObj[key] = val;
+});
+console.log('found keys and values in URL', urlKeyValObj);
+if (urlKeyValObj.access_token) {
+  // we probably got whole new set of authentication data
+  console.log('setting new value as access_token is present');
+  chrome.storage.local.set({ authData: urlKeyValObj });
+}
--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,35 +1,69 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../assets/styles/tailwind.css';
-
 // @ts-ignore
 import secrets from 'secrets'; // works at runtime, need @ts-ignore as a workaround: https://github.com/lxieyang/chrome-extension-boilerplate-react/issues/67
 
 const Popup = () => {
+  const [authData, setAuthData] = useState<{ [key: string]: any }>();
+
   useEffect(() => {
-    document.onpaste = (evt) => {
-      const file = evt.clipboardData?.files[0];
-      if (file) {
-        console.log(file);
-      }
+    const getAuthData = async () => {
+      const authData = (await chrome.storage.local.get(['authData'])).authData;
+      setAuthData(authData);
     };
-    console.log(secrets);
+    getAuthData();
   }, []);
 
-  // TODO: figure out way to get this to work
-  // const clickHandler = useCallback(async () => {
-  //   console.log('handling click');
-  //   document.body.focus();
-  //   document.execCommand('paste');
-  //   const data = await navigator.clipboard.read();
-  //   console.log(data);
-  // }, []);
+  useEffect(() => {
+    if (authData) {
+      console.log('auth data', authData);
+      document.onpaste = async (evt) => {
+        const file = evt.clipboardData?.files[0];
+        if (file) {
+          console.log(file);
+
+          const body = new FormData();
+          body.append('file', new Blob([await file.arrayBuffer()]));
+          body.append('fileName', file.name);
+          body.append('publicKey', secrets.PUBLIC_KEY);
+          body.append(
+            'expire',
+            (Math.floor(Date.now() / 1000) + 60 * 59).toString()
+          );
+
+          fetch('https://upload.imagekit.io/api/v1/files/upload', {
+            body,
+            headers: {
+              Authorization: 'Basic eW91cl9wcml2YXRlX2FwaV9rZXk6',
+              'Content-Type': 'multipart/form-data',
+            },
+            method: 'POST',
+          });
+        }
+      };
+    }
+  }, [authData]);
 
   return (
     <div className="h-screen w-screen flex gap-2 justify-center items-center flex-col">
       <h2 className="text-center">
         Copy and upload an image from your clipboard
       </h2>
-      <span>Hit Ctrl + V to copy an image from your clipboard</span>
+      {authData?.access_token ? (
+        <span>Hit Ctrl + V to copy an image from your clipboard</span>
+      ) : (
+        <span>
+          Please{' '}
+          <a
+            href={`https://api.imgur.com/oauth2/authorize?client_id=${secrets.CLIENT_ID}&response_type=token`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            authenticate
+          </a>{' '}
+          first.
+        </span>
+      )}
       {/* TODO: figure out how to make that work - why is it so difficult loooool */}
       {/* <span>Either hit Ctrl + V or click the button below</span>
       <button
